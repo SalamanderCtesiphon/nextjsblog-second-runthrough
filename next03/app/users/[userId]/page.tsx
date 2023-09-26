372e985d8ac0ca8d501d75206977a9fcc1a4608e
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-25 19:48:31 -0500

    implemented get user posts

--- next03/app/users/[userId]/page.tsx
+++ next03/app/users/[userId]/page.tsx
@@ -1,5 +1,7 @@
 import getUser from "@/lib/getUser"
 import getUserPosts from "@/lib/getUserPosts"
+import { Suspense } from "react"
+import UserPosts from "./components/UserPosts"
 
 type Params = {
   params: {
@@ -10,15 +12,19 @@ type Params = {
 export default async function UserPage({ params: { userId }}: Params) {
 
   const userData: Promise<User> = getUser(userId)
-  const userPostData: Promise<Post[]> = getUserPosts(userId)
+  const userPostsData: Promise<Post[]> = getUserPosts(userId)
 
-  const [user, userPosts] = await Promise.all([userData, userPostData])
+  //const [user, userPosts] = await Promise.all([userData, userPostData])
+  const user = await userData
 
   return (
     <>
       <h2>{user.name}</h2>
       <br />
-      <UserPosts posts={userPosts} />
+      <Suspense fallback={<h2>Loading...</h2>}>
+        <UserPosts promise={userPostsData} />
+      </Suspense>
+     
     </>
   )
 }
