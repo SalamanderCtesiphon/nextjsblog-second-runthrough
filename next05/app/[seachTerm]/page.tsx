d602cd5ba6829536ac477fcc896e352b0c7f63a2
salamanderctesiphon <swbrookshire@gmail.com>
2023-09-26 16:50:08 -0500

    git add .

--- next05/app/[seachTerm]/page.tsx
+++ next05/app/[seachTerm]/page.tsx
@@ -1,5 +1,4 @@
 import getWikiResults from "@/lib/getWikiResults"
-import Item from "./components/Item"
 
 type Props = {
     params: {
@@ -10,7 +9,7 @@ type Props = {
 export async function generateMetadata({ params: { searchTerm } }: Props) {
     const wikiData: Promise<SearchResult> = getWikiResults(searchTerm)
     const data = await wikiData
-    const displayTerm = searchTerm.replaceAll('%20', ' ')
+    const displayTerm = searchTerm/* .replaceAll('%20', ' ') */
 
     if (!data?.query?.pages) {
         return {
@@ -33,7 +32,7 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
         <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
             {results
                 ? Object.values(results).map(result => {
-                    return <Item key={result.pageid} result={result} />
+                    return <p>{JSON.stringify(result)}</p>
                 })
                 : <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
             }
